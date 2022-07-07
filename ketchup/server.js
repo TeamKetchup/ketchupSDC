83cdb28aa260598e96fddab5eac3f7c3df9db69d
bsshapiro <bsshapiro7@gmail.com>
2022-07-07 17:48:24 -0500

    Added the ability to post to S3 bucket

--- ketchup/server.js
+++ ketchup/server.js
@@ -3,18 +3,46 @@ const express = require("express")
 const app = express()
 const db = require("./db/conn")
 const cors = require("cors")
-
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join("./my-app/public"));
-  });
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${fileName}`,
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+
+uploadFile('Garrett.png')
+
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join("./my-app/public"));
+//   });
 
 app.get("/api/products", async (_, res) => {
     try {
