da62e468a9706ae54cf8d817fa3c7de0a39b19c3
Jonathon Wazney <jonathonwazney@Jonathons-Air.lan>
2022-07-14 13:50:05 -0500

    dont merge

--- ketchup/src/components/header.js
+++ ketchup/src/components/header.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css'
 
@@ -7,9 +7,28 @@ import Logo from './LogInSignUp/image-removebg-preview.png'
 function Header() {
     let [login,setLogin] = useState({href: '/Loginpage', li: 'Login/Signin'})
     let [user, setUser] = useState()
-    function userlogin(){
-
+    useEffect(() => {
+        userlogin()
+    
+    }, []);
+    let userlogin = async () => {
+        try {
+            const response = await fetch(`http://localhost:3025/api/profileinfo/1`);
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                )
+            }
+            let actualData = await response.json();
+            setUser(actualData);
+            
+        } catch (error) {
+            console.log(error);
+        }
     }
+   console.log(user)
+
+    
   return (
       <>
       <nav className='navBar'>
@@ -19,7 +38,8 @@ function Header() {
           <input type='text'></input>
           <button>search</button>
           <ul>
-              <li className='active'><Link to={login.href}>{login.li}</Link></li>
+              {user && <li>{user[0].username}</li>}
+              {user &&<li><img src={user[0].avatar} alt='avatar'/></li>}
           </ul>
       </nav>
       
