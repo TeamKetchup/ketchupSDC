8eb38811d71d2a02f61abf34786a8791a188c0ca
greyes92 <gibranreyes92@gmail.com>
2022-08-31 18:15:50 -0500

    server

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(credentials);
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 // app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
